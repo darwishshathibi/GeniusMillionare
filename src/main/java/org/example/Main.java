7b2213daa3e2e034b6f78bb10a5e8fd2283c3df7
darwishathibi <danish.darwis@live.iium.edu.my>
2022-12-15 02:19:26 +0800

    ubah sikit jaa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 public class Main {
-
+    static String[] OneOne = {"Direction only","Magnitude only","Magnitude or direction","Magnitude and direction"};
+    static String[] OneTwo = {"dilation of arterioles near the surface of the skin","erector muscles contract, causing hairs to stand up","rapid contractions of skeletal muscles","sweat glands release less sweat"};
+    static String[] OneThree = {"Carbon dioxide","Sulphur dioxide","Calcium chloride","Hydrogen chloride"};
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
         GameBegins();
     }
 
@@ -27,8 +31,7 @@ public class Main {
     static void GameBegins(){
         GameRules();
         String inputname = StudentName();
-        System.out.println(inputname);
-
+        Questions();
     }
 
     static String StudentName(){
@@ -37,4 +40,68 @@ public class Main {
         String name = input.nextLine();
         return name;
     }
+
+    static void Questions(){
+        String[] LevelOne = {"A vector quantity has:","Which substance consists of ion?","Correctly describes how an endotherm would respond to an increase in temperature?"};
+        String[] LevelTwo = {"Which of the following has the lowest rate of reaction?","What is the meaning of 800 W?",".Which of the following, A to D, is a similarity in the way ATP is made in respiration and photosynthesis?"};
+        String[] LevelThree = {"Which of the following is a reduction reaction:","Which statement is correct about n-type semiconductor?","Which of the following, A to D, is not an example of cell signalling?"};
+        String[] LevelFour = {"Which radioactive can be used to determine the age of the fossil?","Which of the following statements, A to D, is evidence for geotropism?","What type of glass is formed when calcium carbonate is heated with silicon dioxide?"};
+        String[] LevelFive = {"Which of the following properties of the hormone hCG allows it to be detected in urine?","What is the oxidation number of X in XO,?","Which value is equal to 3 500 000 W?"};
+        System.out.println("The question is now begin.");
+        QueOne(LevelOne);
+//        QueTwo(LevelTwo);
+//        QueThree(LevelThree);
+//        QueFour(LevelFour);
+//        QueFive(LevelFive);
+    }
+
+    static void QueOne(String[] q){
+        List<String> list = Arrays.asList(q); // converting array to a List
+        Collections.shuffle(list); // Shuffling list elements
+        String ques = q[0];
+        System.out.println("\nQuestion 1 (Child)\n\t"+ques);
+        AnswerAll(ques);
+    }
+
+    static void AnswerAll(String q){
+        Scanner input = new Scanner(System.in);
+        //prompt user enter answer
+        char mcq = 64;
+
+        if (q == "A vector quantity has:") {
+            List<String> list = Arrays.asList(OneOne); // converting array to a List
+            Collections.shuffle(list); // Shuffling list elements
+            for (int i = 0; i < OneOne.length; i++) {
+                mcq++;
+                System.out.println("\t\t"+mcq+" "+OneOne[i]);
+            }
+        } else if (q == "Which substance consists of ion?") {
+            List<String> list = Arrays.asList(OneThree); // converting array to a List
+            Collections.shuffle(list); // Shuffling list elements
+            for (int i = 0; i < OneThree.length; i++) {
+                mcq++;
+                System.out.println("\t\t"+mcq+" "+OneThree[i]);
+            }
+        } else if (q == "Correctly describes how an endotherm would respond to an increase in temperature?") {
+            List<String> list = Arrays.asList(OneTwo); // converting array to a List
+            Collections.shuffle(list); // Shuffling list elements
+            for (int i = 0; i < OneTwo.length; i++) {
+                mcq++;
+                System.out.println("\t\t" + mcq + " " + OneTwo[i]);
+            }
+        }
+        System.out.print("Enter your answer: ");
+        char ansmcq = input.next().charAt(0);
+       // char trueans = ProcessAnswer(ansmcq);
+    }
+    
+   /*static boolean ProcessAnswer(char a){
+        String[] trueq1 ={"Magnitude and direction","dilation of arterioles near the surface of the skin","Calcium chloride"};
+        if (a == ){
+
+        } else if () {
+
+        }
+    }*/
+
 }
